25dc350a1966baa42513a356d84f92c517bde2c3
mio.app <a3ks7606@yahoo.co.jp>
2019-03-17 14:22:33 +0900

    完成

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -1,4 +1,5 @@
 
+
 //
 //  ViewController.swift
 //  SlideshowApp
@@ -12,99 +13,127 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var PlayPause: UIButton!
+    @IBOutlet weak var Prev: UIButton!
+    @IBOutlet weak var Next: UIButton!
     
     let image = ["img1.jpg","img2.jpg","img3.jpg"]
-    
-    
-    var counter:Int = 0
-    
     let imageNameArray = [
         "img1.jpg",
         "img2.jpg",
         "img3.jpg",
         ]
     
+    var timer: Timer!
+    var counter:Int = 0
+    var dispImageNo = 0
+    
+    //MARK:- override
+    //    --------------------------------------------------------------------------------
+    //    viewDidLoad()
+    //    --------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         let name = imageNameArray[dispImageNo]
         let image = UIImage(named: name);
         
         imageView.image = image
+        print(timer)
         
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
-    @IBOutlet weak var PlayPause: UIButton!
-    
-    @IBOutlet weak var Prev: UIButton!
-    
-    @IBOutlet weak var Next: UIButton!
-    
-    var timer: Timer!
-    
-    
-    //再生ボタンを押した時のメソッド
-    @IBAction func PlayPause(_ sender: Any) {
-        if self.timer != nil {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let ResultViewController = segue.destination as! ResultViewController
+        ResultViewController.image = imageView.image
         
-       
+    }
     
-            PlayPause.setTitle("再生", for: UIControl.State.normal);            timer?.invalidate()
+    //MARK:- method
+    //    --------------------------------------------------------------------------------
+    //    displayImage()
+    //    --------------------------------------------------------------------------------
+    @objc func displayImage() {
+        dispImageNo += 1
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        print(dispImageNo)
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
+    }
+    
+    //MARK:- action
+    //    --------------------------------------------------------------------------------
+    //    PlayPause(_ sender: Any)
+    //    --------------------------------------------------------------------------------
+    @IBAction func PlayPause(_ sender: Any) {
+        print(timer)
+        if self.timer != nil {
+            PlayPause.setTitle("再生", for: UIControl.State.normal)
+            timer?.invalidate()
             timer = nil
-            
-        }else {
+            Prev.isEnabled = true
+            Next.isEnabled = true
+        } else {
             PlayPause.setTitle("停止", for: UIControl.State.normal)
-            
-            
-             Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: (#selector(displayImage
-                )), userInfo: nil, repeats: true)
-
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: (#selector(displayImage)), userInfo: nil, repeats: true)
+            //             Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: (#selector(displayImage)), userInfo: nil, repeats: true)
             Prev.isEnabled = false
-            
             Next.isEnabled = false
-            
-            
-            
-            
         }
     }
-    
-    
-    
     
     @IBAction func onPrev(sender: AnyObject) {
         
         // 表示している画像の番号を1減らす
         dispImageNo -= 1
         
-        // 表示している画像の番号を元に画像を表示する
-        displayImage()
+        // 範囲より下を指している場合、最後の画像を表示
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+        }
+        
+        // 表示している画像の番号から名前を取り出し
+        let name = imageNameArray[dispImageNo]
+        
+        // 画像を読み込み
+        let image = UIImage(named: name)
+        
+        // Image Viewに読み込んだ画像をセット
+        imageView.image = image
     }
+    
     @IBAction func onNext(sender: AnyObject) {
         
         // 表示している画像の番号を1増やす
         dispImageNo += 1
-        
-        // 表示している画像の番号を元に画像を表示する
-        displayImage()
-    }
-    
-    /// 表示している画像の番号
-    var dispImageNo = 0
-    
-    /// 表示している画像の番号を元に画像を表示する
-    @objc func displayImage() {
-        print("動作確認")
-        // 画像の名前の配列
-        //            let imageNameArray = [
-        //                "img1",
-        //                "img2",
-        //                "img3",
-        //                ]
-        
         
         // 範囲より下を指している場合、最後の画像を表示
         if dispImageNo < 0 {
@@ -129,26 +158,7 @@ class ViewController: UIViewController {
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
     
-    
-    
     @IBAction func CloseUp(_ sender: Any) {
-        
-        
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        let ResultViewController = segue.destination as! ResultViewController
-        
-        ResultViewController.image = imageView.image
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     
 }
